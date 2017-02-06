89abd8e26fdd8e9927c332dd6cd4fa5a3d977f0c
HolyBreadstick <bjm19@pct.edu>
2017-02-06 14:08:12 -0500

    Ended my work at section with search helper: 'As well the “add-migration InitialCreate”'

---
+++ WebAPI/Structure/ApplicationDbContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNet.Identity.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebAPI.Structure
+{
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext() : base("Connection", throwIfV1Schema: false)
+        {
+            Configuration.ProxyCreationEnabled = true;
+            Configuration.LazyLoadingEnabled = true;
+        }
+
+        public static ApplicationDbContext Create()
+        {
+            return new ApplicationDbContext();
+        }
+    }
+}
---
+++ WebAPI/Structure/ApplicationUser.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNet.Identity.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace WebAPI.Structure
+{
+    public class ApplicationUser : IdentityUser
+    {
+        [Required]
+        [MaxLength(100)]
+        public String FirstName { get; set; }
+        
+        [Required]
+        [MaxLength(100)]
+        public String LastName { get; set; }
+
+        [Required]
+        public byte Level { get; set; }
+
+        [Required]
+        public DateTime JoinDate { get; set; }
+    }
+}
